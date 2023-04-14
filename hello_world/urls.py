8f3abf9a8ebc07ea872fb075bb2586b087e49171
rogamestica <rogamestica.pascual815@gmail.com>
2023-04-14 12:03:04 +0800

    add index

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from hello_world import views
+from . import views
 
 urlpatterns = [
-    path('', views.hello_world, name='hello_world'),
+    path('', views.index, name='Home'),
+    path('hello world', views.hello_world, name='hello_world'),
+    path('image/', views.image_view, name='image-view'),
+    
+ 
+
 ]
--- hello_world/views.py
+++ hello_world/views.py
@@ -3,6 +3,13 @@
 
 from django.shortcuts import render
 
+
+def index(request):
+    return render(request, 'index.html', {})
+
 def hello_world(request):
     return render(request, 'hello_world.html', {})
+
+def image_view(request):
+    return render(request, 'image.html')
 
